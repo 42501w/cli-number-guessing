13654e8b2bf3eb0a409d39c1cbce9dd7de01ccfd
42501w <syedaryana869@gmail.com>
2024-03-26 05:44:45 +0500

    ceated number guessing game

---
+++ index.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+
+const answers = await inquirer.prompt([
+    {
+        name: "userGuesssedNumber",
+        type: 'number',
+        message: "please guess a number between 1-10: " ,
+    }
+]);
+
+if (answers.userGuesssedNumber === randomNumber) {
+    console.log("Congratulations! You guessed a right number");
+} else {
+    console.log("You guessed wrong number");
+}
+
